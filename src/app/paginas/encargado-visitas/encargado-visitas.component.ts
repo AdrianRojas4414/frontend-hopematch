1086b530d78e612d0cd6dc6e7ac8cfdbe5ed2515
Reynaldo <reynaldo.ruiz@ucb.edu.bo>
2025-05-08 10:29:18 -0400

    implementacion de estructura para vista de visitas encargado-visitas

---
+++ src/app/paginas/encargado-visitas/encargado-visitas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encargado-visitas',
+  imports: [],
+  templateUrl: './encargado-visitas.component.html',
+  styleUrl: './encargado-visitas.component.scss'
+})
+export class EncargadoVisitasComponent {
+
+}
