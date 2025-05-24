1e9f04161a9f6be4c2294cb748fbe2624a79814d
Diegodenac <142777875+Diegodenac@users.noreply.github.com>
2025-05-24 11:40:57 -0400

    HM5-09 UI Design Changes. Refactors in Landing Page and Log In - Sign Up Forms (#48)
    * simple refactor
    
    * Refactors and login changes, input for forms components added
    
    * Changes in Principal Home
    
    * Changes in Register UI
    
    * Refactoring NavBar
    
    * Refactor in about us section
    
    * Changes in UI LogIn
    
    * Form register padrino updated
    
    * Sign Up Encargado Updated
    
    * Sign Up Admin Updated
    
    * Refactor merge errors
    
    * Changes in Home Padrino, modified the cards for encargados and donaciones, changed navbar, footer added, better layout
    
    * Change color typography, done card donacion
    
    * done hogar details card
    
    * REFACTOR: Home Padrino UI improvements

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -20,12 +20,14 @@ export const TEXTOS = {
     aboutVisionText: "Construir un mundo donde cada niño tenga acceso a oportunidades, recursos y una red de apoyo afectiva.",
     // NAV BUTTONS
     buttonHome: 'Home',
-    buttonAboutUs: 'AboutUs',
+    buttonAboutUs: 'About Us',
     buttonSignUp: 'Sign Up',
     buttonLogIn: 'Log In',
     buttonContact: 'Contact',
     buttonVolver: 'Volver a Home',
     buttonCerrarSesion: 'Cerrar Sesion',
+    buttonPerfil: 'Perfil',
+    buttonSignOut: 'Sign Out',
     // SIGN UP OPTIONS
     padrinoOption: 'QUIERO SER PADRINO',
     encargadoOption: 'QUIERO REGISTRAR A MI HOGAR',
